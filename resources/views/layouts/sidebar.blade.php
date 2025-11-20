03167f235be50cce7f778ff1b7d0730d5c0dd03d
N753642 <mnajib753642@gmail.com>
2025-11-20 08:28:43 +0800

    sb admin tes

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,20 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Branding -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/') }}">
+        <div class="sidebar-brand-text mx-3">Penjualan</div>
+    </a>
+
+    <hr class="sidebar-divider my-0">
+
+    <!-- Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('produk.index') }}">
+            <i class="fas fa-fw fa-tshirt"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+
+    <hr class="sidebar-divider d-none d-md-block">
+
+</ul>
--- resources/views/produk/index.blade.php
+++ resources/views/produk/index.blade.php
@@ -1,5 +1,4 @@
-@extends('layouts.app')
-
+@extends('layouts.sbadmin')
 @section('content')
 
 <div class="d-flex justify-content-between align-items-center mb-3">
